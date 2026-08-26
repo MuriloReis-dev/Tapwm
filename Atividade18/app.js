1672f9c4bbbf8453faad1d82a0256cf6644ae9b0
Murilo Reis Schiavotto <129551298+MuriloReis-dev@users.noreply.github.com>
2026-08-26 14:35:28 -0300

    Add files via upload

---
+++ Atividade18/app.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, function() {
+    console.log("servidor com express for carregado");
+});
+
+app.get('/', function(req, res) {
+    res.send("<html><body>Site da Fatec Sorocaba</body></html>");
+});
+
+app.get('/historia', function(req, res) {
+    res.render("secao/historia");
+});
+
+app.get('/cursos', function(req, res) {
+    res.render("secao/cursos");
+});
+
+app.get('/professores', function(req, res) {
+    res.render("secao/professores");
+});
